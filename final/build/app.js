1d92ce62455e4efed5f47bd7789018847ab27a96
CeciliaxChen <cc3875@drexel.edu>
2022-11-28 16:05:17 -0500

    bolding menu fonts

--- final/build/app.js
+++ final/build/app.js
@@ -17,28 +17,49 @@ hoverTriggerElement.onmouseout = function (event) {
 
 
 
-var element1 = document.getElementById("onBold");
-var element2 = document.getElementById("onBold2");
-var element3 = document.getElementById("onBold3");
+// var element1 = document.getElementById("onBold");
+// var element2 = document.getElementById("onBold2");
+// var element3 = document.getElementById("onBold3");
 
-function changeStyle(){
-  console.log('change style');
-  // example of adding a CSS style to the text 
-  // element1.classList.add("font1-bold");
-  element1.style.fontWeight = "bolder";
-  element2.style.fontWeight = "normal";
-  element3.style.fontWeight = "normal";
-}
-function changeStyle2(){
-  element2.style.fontWeight = "bolder";
-  element1.style.fontWeight = "normal";
-  element3.style.fontWeight = "normal";
-}
-function changeStyle3(){
-  element3.style.fontWeight = "bolder";
-  element1.style.fontWeight = "normal";
-  element2.style.fontWeight = "normal";
-}
+// function changeStyle(){
+//   console.log('change style');
+//   // example of adding a CSS style to the text 
+//   // element1.classList.add("font1-bold");
+//   element1.style.fontWeight = "bolder";
+//   element2.style.fontWeight = "normal";
+//   element3.style.fontWeight = "normal";
+// }
+// function changeStyle2(){
+//   element2.style.fontWeight = "bolder";
+//   element1.style.fontWeight = "normal";
+//   element3.style.fontWeight = "normal";
+// }
+// function changeStyle3(){
+//   element3.style.fontWeight = "bolder";
+//   element1.style.fontWeight = "normal";
+//   element2.style.fontWeight = "normal";
+// }
+
+
+
+// const sampleTextObj = document.querySelector("#sampleText");
+// const boldFontObj = document.querySelector(".boldFont");
+
+// sampleTextObj.addEventListener("click", () => {
+//   boldFontObj.classList.toggle("be-bold");
+// });
+
+
+const menuItem1Obj = document.querySelector("#menu_item1");
+const boldFont1Obj = document.querySelector(".boldFont1");
+
+menuItem1Obj.addEventListener("click", () => {
+  console.log('THIS WORKS');
+  boldFont1Obj.classList.toggle("font1-be-bold");
+});
+
+
+
 
 
 
