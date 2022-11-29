607873b9d798618be3417196454f072d1cbd4c34
CeciliaxChen <cc3875@drexel.edu>
2022-11-29 12:11:13 -0500

    updates

--- final/build/app.js
+++ final/build/app.js
@@ -17,9 +17,9 @@ hoverTriggerElement.onmouseout = function (event) {
 
 
 
-// var element1 = document.getElementById("onBold");
-// var element2 = document.getElementById("onBold2");
-// var element3 = document.getElementById("onBold3");
+// var element1 = document.getElementById("menu_item1");
+// var element2 = document.getElementById("menu_item2");
+// var element3 = document.getElementById("menu_item3");
 
 // function changeStyle(){
 //   console.log('change style');
@@ -30,11 +30,13 @@ hoverTriggerElement.onmouseout = function (event) {
 //   element3.style.fontWeight = "normal";
 // }
 // function changeStyle2(){
+//   console.log('change style');
 //   element2.style.fontWeight = "bolder";
 //   element1.style.fontWeight = "normal";
 //   element3.style.fontWeight = "normal";
 // }
 // function changeStyle3(){
+//   console.log('change style');
 //   element3.style.fontWeight = "bolder";
 //   element1.style.fontWeight = "normal";
 //   element2.style.fontWeight = "normal";
@@ -54,11 +56,27 @@ const menuItem1Obj = document.querySelector("#menu_item1");
 const boldFont1Obj = document.querySelector(".boldFont1");
 
 menuItem1Obj.addEventListener("click", () => {
-  console.log('THIS WORKS');
+  // console.log('THIS WORKS');
   boldFont1Obj.classList.toggle("font1-be-bold");
 });
 
 
+const menuItem2Obj = document.querySelector("#menu_item2");
+const boldFont2Obj = document.querySelector(".boldFont2");
+
+menuItem2Obj.addEventListener("click", () => {
+  console.log('THIS WORKS');
+  boldFont2Obj.classList.toggle("font2-be-bold");
+});
+
+
+const menuItem3Obj = document.querySelector("#menu_item3");
+const boldFont3Obj = document.querySelector(".boldFont3");
+
+menuItem3Obj.addEventListener("click", () => {
+  console.log('THIS WORKS');
+  boldFont3Obj.classList.toggle("font3-be-bold");
+});
 
 
 
@@ -68,4 +86,12 @@ menuItem1Obj.addEventListener("click", () => {
 
 // DyslexicButton.addEventListener("click", () => {
 //   DyslexicFont.classList.toggle("be-bold");
+// });
+
+
+
+// const sampleTextObj = document.querySelector("boldFont1");
+// sampleTextObj.addEventListener("click", () => {
+//   console.log("sample text clicked on");
+//   sampleTextObj.classList.toggle("fontChange1");
 // });
